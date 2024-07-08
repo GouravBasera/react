81e863eca78c9353e77682481b782520979490e8
Gourav Basera <gouravbasera06@gmail.com>
2024-07-08 21:55:46 +0530

    Added React router project

--- 04reactrouter/src/index.js
+++ 04reactrouter/src/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import {Layout, Home, About, Contact, User, Github} from './components/route'
+import { githubInfoLoader } from './components/Github/Github';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+    {
+      path: '',
+      element: <Home/>,
+    },
+    {
+      path: 'about',
+      element: <About/>
+    },
+    {
+      path: 'contact',
+      element: <Contact/>
+    },
+    {
+      path: 'user/:userid',
+      element: <User/>,
+    },
+    {
+      loader: githubInfoLoader,
+      path: 'github',
+      element: <Github/>,
+    }
+  ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
